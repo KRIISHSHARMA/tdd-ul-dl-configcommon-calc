7df3f635a8513be6713102619b0ba3fe85dbf35e
Kriish sharma <kriish.sharma2006@gmail.com>
2023-12-31 14:56:39 +0530

    commit

--- calc.py
+++ calc.py
@@ -1,1 +1,24 @@
-
+class Slotconfig : 
+    def __init__(self , ds , us , tp , scs):
+        self.ds = ds
+        self.us = us
+        self.tp = tp
+        self.scs = scs
+    def slotc(self):
+        ts = self.tp * (2**self.scs)
+        spc = ts - (self.ds+self.us)
+        l = []
+        for i in range(int(self.ds)) : 
+            l.append('D')
+        for i in range(int(spc)) :
+            l.append('S')
+        for i in range(int(self.us)) :
+            l.append('U')
+        
+        print(l)
+ds = int(input("ENTER DL SLOT ::  "))
+us = int(input("ENTER UL SLOT ::  "))
+tp = float(input("ENTER TRASMISSION PERIODICITY :: "))
+scs = int(input("ENTER SCS (0 = 15 , 1 = 30 , 2 = 60 , 3 = 120) :: "))
+pattern1 = Slotconfig(ds,us,tp,scs)
+pattern1.slotc()
